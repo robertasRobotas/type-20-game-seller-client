8aad22b133c130f47118fba53101b8c321fcc3f9
Robertas <r.ankudovicius@gmail.com>
2024-05-29 08:56:57 +0300

    Added new server url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3002",
+    // SERVER_URL: "http://localhost:3002",
+    SERVER_URL: "https://type-20-game-seller.onrender.com",
   },
 };
 
